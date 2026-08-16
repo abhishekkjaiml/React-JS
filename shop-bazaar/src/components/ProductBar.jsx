8be2e9d44557c0285740e5cfde0a106bb14d7abe
Abhishek Kumar Jaiswar <abhishekkjaiml@gmail.com>
2026-08-16 10:07:37 +0530

    PB update

--- shop-bazaar/src/components/ProductBar.jsx
+++ shop-bazaar/src/components/ProductBar.jsx
@@ -3,9 +3,7 @@ import KeyboardArrowDownOutlinedIcon from "@mui/icons-material/KeyboardArrowDown
 import { NavLink } from "react-router-dom";
 
 const ProductBar = () => {
-
   const getStyle = ({ isActive }) => {
-
     const styles =
       "relative cursor-pointer transition-colors duration-200 after:absolute after:-bottom-1 after:left-0 after:h-0.5 after:bg-primary after:transition-all after:duration-200";
 
@@ -14,84 +12,47 @@ const ProductBar = () => {
       : `text-text-primary hover:text-primary hover:after:w-full after:w-0 ${styles}`;
   };
 
-
   return (
     <header className="flex h-12 w-screen flex-wrap bg-background-soft font-semibold">
-
       <ul className="flex w-screen flex-wrap items-center">
-
         {/* ================= CATEGORY ================= */}
 
         <div className="mx-10 flex h-full w-35 flex-wrap items-center border-r border-border">
-
           <li className="flex items-center gap-2 text-text-primary">
-
             <DensityMediumOutlinedIcon />
-
             Category
-
             <KeyboardArrowDownOutlinedIcon />
-
           </li>
-
         </div>
-
 
         {/* ================= NAVIGATION ================= */}
 
         <div className="flex flex-wrap gap-7">
-
-          <NavLink
-            to="/"
-            className={getStyle}
-          >
+          <NavLink to="/" className={getStyle}>
             Home
           </NavLink>
 
-
-          <NavLink
-            to="/shop"
-            className={getStyle}
-          >
+          <NavLink to="/shop" className={getStyle}>
             Shop
           </NavLink>
 
-
-          <NavLink
-            to="/deals"
-            className={getStyle}
-          >
+          <NavLink to="/deals" className={getStyle}>
             Deals
           </NavLink>
 
-
-          <NavLink
-            to="/new_arrivels"
-            className={getStyle}
-          >
+          <NavLink to="/new_arrivels" className={getStyle}>
             New Arrivals
           </NavLink>
 
-
-          <NavLink
-            to="/best_sellers"
-            className={getStyle}
-          >
+          <NavLink to="/best_sellers" className={getStyle}>
             Best Sellers
           </NavLink>
 
-
-          <NavLink
-            to="/brands"
-            className={getStyle}
-          >
+          <NavLink to="/brands" className={getStyle}>
             Brands
           </NavLink>
-
         </div>
-
       </ul>
-
     </header>
   );
 };
