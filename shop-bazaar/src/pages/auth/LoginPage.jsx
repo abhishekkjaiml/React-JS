a8c9420c6721d5f9b3750989ecc07d52d125e8e6
Abhishek Kumar Jaiswar <abhishekkjaiml@gmail.com>
2026-09-12 21:18:16 +0530

    .

--- shop-bazaar/src/pages/auth/LoginPage.jsx
+++ shop-bazaar/src/pages/auth/LoginPage.jsx
@@ -3,7 +3,7 @@ import Login from '../../components/Login'
 
 const LoginPage = () => {
   return (
-    <div  className='flex flex-wrap justify-center items-center p-10'>
+    <div  >
       <Login />
     </div>
   )
